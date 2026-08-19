f7283683a925bb37e0eb52024140d257078a638f
pragatikadam <kadampragati@gmail.com>
2026-08-19 22:29:00 +0530

    SmallestBiggestNumber

---
+++ src/com/my/code/javaCollection/examples/SmallestBiggestNumber.java
@@ -0,0 +1,23 @@
+package com.my.code.javaCollection.examples;
+
+public class SmallestBiggestNumber {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {-5, 2, 9, 1, 7};
+
+        int smallest = numbers[0];
+        int biggest = numbers[0];
+        for (int num : numbers) {
+            if (smallest > num) {
+                smallest = num;
+            }
+            if (biggest < num) {
+                biggest = num;
+            }
+        }
+
+        System.out.println("Smallest number: " + smallest);
+        System.out.println("Biggest number: " + biggest);
+    }
+}
