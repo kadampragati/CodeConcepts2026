b7e6bccae2b999fd17d41b25d9231e89df5263db
pragatikadam <kadampragati@gmail.com>
2026-08-20 21:28:46 +0530

    rotate array

--- src/com/my/code/javaCollection/examples/SmallestBiggestNumber.java
+++ src/com/my/code/javaCollection/examples/SmallestBiggestNumber.java
@@ -4,12 +4,14 @@ public class SmallestBiggestNumber {
 
     public static void main(String[] args) {
 
-        int[] numbers = {-5, 2, 9, 1, 7};
+        int[] numbers = {-5, 2, 9, 1, -7};
 
         int smallest = numbers[0];
+        int smaller = numbers[0];
         int biggest = numbers[0];
         for (int num : numbers) {
             if (smallest > num) {
+                smaller = smallest;
                 smallest = num;
             }
             if (biggest < num) {
@@ -18,6 +20,7 @@ public class SmallestBiggestNumber {
         }
 
         System.out.println("Smallest number: " + smallest);
+        System.out.println("Second smallest number: " + smaller);
         System.out.println("Biggest number: " + biggest);
     }
 }
